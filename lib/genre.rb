0c4a7ccb23d0b9296e69ad0b8fda4e0513099ab0
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-21 22:56:34 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -48,55 +48,55 @@
   
 # end
 
-class Genre
+# class Genre
 
-  #extend Concerns::Findable
+#   #extend Concerns::Findable
 
-  attr_accessor :name
-  @@all = []
+#   attr_accessor :name
+#   @@all = []
 
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
+#   def initialize(name)
+#     @name = name
+#     @songs = []
+#   end
 
-  def self.all
-    @@all
-  end
+#   def self.all
+#     @@all
+#   end
 
-  def self.destroy_all
-    @@all.clear
-  end
+#   def self.destroy_all
+#     @@all.clear
+#   end
 
-  def save
-    @@all << self
-  end
+#   def save
+#     @@all << self
+#   end
 
-  def self.create(name)
-    self.new(name).tap do |genre|
-      genre.save
-    end
-  end
+#   def self.create(name)
+#     self.new(name).tap do |genre|
+#       genre.save
+#     end
+#   end
 
-  def songs
-    @songs
-  end
-
-
-  def add_song(song)
-    song.genre = self unless song.genre == self
-    @songs << song unless @songs.include?(song)
-  end
+#   def songs
+#     @songs
+#   end
 
 
-  def artists
-    artists = @songs.collect do |song|
-      song.artist
-    end
-    artists.uniq
-  end
+#   def add_song(song)
+#     song.genre = self unless song.genre == self
+#     @songs << song unless @songs.include?(song)
+#   end
 
-end
+
+#   def artists
+#     artists = @songs.collect do |song|
+#       song.artist
+#     end
+#     artists.uniq
+#   end
+
+# end
 
 
 
