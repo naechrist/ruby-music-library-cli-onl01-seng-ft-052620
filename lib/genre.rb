10592bc830e08a1a5d57519ca95a84f650446339
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-21 22:35:26 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,56 @@
+class Genre 
+  attr_accessor :name, :songs
+  @@all = [] 
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+    save 
+  end
+  
+  def artists
+    @new_array = []
+    @songs.each do |song|
+      if @new_array.include?(song.artist)
+        nil
+      else
+        @new_array << song.artist
+      end
+    end
+    @new_array
+  end
+  
+  def songs 
+    @songs 
+  end
+  
+  def save 
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def self.destroy_all 
+    @@all.clear 
+  end
+  
+  def self.count 
+    @@all.size 
+  end
+  
+    def self.create(genre)
+    genre = Genre.new(genre)
+    genre.save
+    genre
+  end
+  
+end
+
+
+
+
+
+
+
