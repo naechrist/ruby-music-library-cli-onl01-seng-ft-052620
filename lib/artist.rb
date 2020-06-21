a6a6850e25bb871fb740aac6757b56c141751811
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-21 23:04:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,112 +1,3 @@
-# class Artist 
-#   attr_accessor :name, :songs
-#   @@all = [] 
-  
-#   def initialize(name)
-#     @name = name
-#     @songs = []
-#     save 
-#   end
-  
-#   def songs
-#     @songs
-#   end
-  
-#   def add_song(song)
-#     if song.artist == nil 
-#     song.artist = self 
-#     end
-#       if @songs.include?(song)
-#         nil 
-#       else
-#       @songs << song
-#       end
-#   end
-  
-#   def save 
-#     @@all << self 
-#   end
-  
-#   def self.all 
-#     @@all 
-#   end
-  
-#   def self.destroy_all 
-#     @@all.clear 
-#   end
-  
-#   def self.count 
-#     @@all.size 
-#   end
-  
-#     def self.create(artist)
-#     artist = self.new(artist)
-#     artist.save
-#     artist
-#   end
-  
-#   def genres
-#     @new_array = []
-#     @songs.each do |song|
-#       if @new_array.include?(song.genre)
-#         nil
-#       else
-#         @new_array << song.genre
-#       end
-#     end
-#     @new_array
-#   end
-  
-# end
-# class Artist
-
-#   #extend Concerns::Findable
-
-#   attr_accessor :name
-#   @@all = []
-
-#   def initialize(name)
-#     @name = name
-#     @songs = []
-#   end
-
-#   def self.all
-#     @@all
-#   end
-
-#   def self.destroy_all
-#     @@all.clear
-#   end
-
-#   def save
-#     @@all << self
-#   end
-
-#   def self.create(name)
-#     self.new(name).tap do |artist|
-#       artist.save
-#     end
-#   end
-
-#   def songs
-#     @songs
-#   end
-
-#   def add_song(song)
-#     song.artist = self unless song.artist == self
-#     @songs << song unless @songs.include?(song)
-#   end
-
-#   def genres
-#     genres = @songs.collect do |song|
-#       song.genre
-#     end
-#     genres.uniq
-#   end
-
-
-
-# end
 class Artist
   extend Concerns::Findable
   attr_accessor :name
@@ -117,7 +8,6 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-    # self.save
   end
 
   def self.all
@@ -143,15 +33,11 @@ class Artist
   end
 
   def add_song(song)
-
       song.artist = self unless song.artist
       songs << song unless songs.include?(song)
-      # @songs.uniq!
-
   end
 
   def genres
-    # self.songs.collect {|song| song.genre}
     songs.collect { |song| song.genre}.uniq
   end
 end
